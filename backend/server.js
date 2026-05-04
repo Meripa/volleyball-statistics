b6ebb19ca79f550b3b46e399a41fc97efd0e08ba
Meris <19meris@gmail.com>
2026-05-04 14:20:16 +0300

    Changed port for render

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -187,5 +187,5 @@ app.get("/test-db", async (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Server jookseb siin ilusti http://localhost:${PORT}`)
+  console.log(`Server running on ${PORT}`)
 })
