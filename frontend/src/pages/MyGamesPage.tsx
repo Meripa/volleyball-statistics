dd1123d39db2bf2cad311e79e97cc891ce2c10a9
Meris <19meris@gmail.com>
2026-05-12 20:37:24 +0300

    Fixed mygamespages

---
+++ frontend/src/pages/MyGamesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyGames from "../components/MyGames"
+
+const MyGamesPages = () => {
+  return (
+    <>
+    <MyGames/>
+    </>
+  )
+}
+
+export default MyGamesPages
